ccdfef6bd796937da1a9fa6dc7473e86f55bbb75
Janel Grant <janelgr5@gmail.com>
2018-04-29 20:59:00 -0400

    styling

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import * as firebase from 'firebase';
+import React, { Component } from 'react'
+import { Text, View, ScrollView } from 'react-native'
+import * as firebase from 'firebase'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDHndLmDhxET-Q74ZivpuNPQqex3knh4FU",
@@ -9,17 +9,17 @@ const firebaseConfig = {
   projectId: "couponer-87cbc",
   storageBucket: "couponer-87cbc.appspot.com",
   messagingSenderId: "959778964098"
-};
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+}
+const firebaseApp = firebase.initializeApp(firebaseConfig)
 
 const styles = require('./styles.js')
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>This is an App</Text>
       </View>
-    );
+    )
   }
 }
