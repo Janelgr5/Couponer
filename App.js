31da6becfaba5422d76490c2b558effdfa33c94f
Janel Grant <janelgr5@gmail.com>
2018-04-29 21:05:59 -0400

    actionButton component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
--- components/ActionButton.js
+++ components/ActionButton.js
@@ -1,4 +1,18 @@
 import React, { Component } from 'react'
-import ReactNative from 'react-native'
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native'
 const styles = require('../styles')
 const constants = styles.constants
+
+export default class ActionButton extends Component {
+    render() {
+        return (
+            <View style={styles.action}>
+                <TouchableHighlight
+                    underlayColor={constants.actionColor}
+                    onPress={this.props.onPress}>
+                    <Text style={styles.actionText}>{this.props.title}</Text>
+                </TouchableHighlight>
+            </View>
+        )
+    }
+}
