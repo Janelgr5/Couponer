f906956e8eb34b1d565254b2825047c8984eb967
Janel Grant <janelgr5@gmail.com>
2018-04-29 20:49:53 -0400

    firebase setup

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHndLmDhxET-Q74ZivpuNPQqex3knh4FU",
+  authDomain: "couponer-87cbc.firebaseapp.com",
+  databaseURL: "https://couponer-87cbc.firebaseio.com",
+  projectId: "couponer-87cbc",
+  storageBucket: "couponer-87cbc.appspot.com",
+  messagingSenderId: "959778964098"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const styles = require('./styles.js')
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>This is an App</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
