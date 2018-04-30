da8773f09a22cc8106423144ff28ef282444f0dc
Janel Grant <janelgr5@gmail.com>
2018-04-29 21:09:41 -0400

    ListItem component

---
+++ components/ListItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { View, TouchableHighlight, Text } from 'react-native'
+const styles = require('../styles')
+
+export default class ListItem extends Component {
+    render() {
+        return (
+            <TouchableHighlight onPress={this.props.onPress}>
+                <View style={styles.li}>
+                    <Text style={styles.liText}>{this.props.item.title}</Text>
+                </View>
+            </TouchableHighlight>
+        )
+    }
+}
